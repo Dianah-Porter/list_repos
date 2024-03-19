5cd0818c1a409b081dcc53eeb0b9c9e4d0f453db
Diane <dianahporter@gmail.com>
2024-03-19 17:10:04 +0200

    My second commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter the user name: ")
+age = int(input("Enter your age: "))
+location = input("Enter the location: ")
+print(f"Hello {name}, you are {age} years old and live in {location}.")
